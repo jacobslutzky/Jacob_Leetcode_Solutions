7010b848e8f1f706a4ad89403efd0aa3acb36526
jacobslutzky <69685261+jacobslutzky@users.noreply.github.com>
2024-04-22 17:47:05 -0400

    Time: 1132 ms (28.13%), Space: 20.8 MB (26.73%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,28 +2,24 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         output = []
-        i = 0
+        i=0
         while i < len(nums)-2:
-            low = i+1
+            low=i+1
             high = len(nums)-1
             while low < high:
-                if nums[i] + nums[low] + nums[high] == 0:
-                    output.append([nums[i],nums[low],nums[high]])
-                
-                    while low < len(nums)-1 and nums[low] == nums[low+1]:
-                        low += 1
-                    while high > 0 and nums[high] == nums[high-1]:
-                        high -= 1
-                    low += 1
-                    high -= 1  
-                    
-                elif nums[i] + nums[low] + nums[high] < 0:
+                if high < len(nums)-1 and nums[high] == nums[high+1]:
+                    high-=1
+                    continue
+                three_sum = nums[low] + nums[high]+nums[i]
+                if three_sum > 0:
+                    high-=1
+                elif three_sum < 0:
+                    low +=1
+                else:   
+                    output.append([nums[i], nums[low],nums[high]])
                     low+=1
-                else:
-                    high -= 1
-            
-            while i < len(nums)-1 and nums[i] == nums[i+1]:
-                i+=1
+                    high-=1
             i+=1
-        
+            while i < len(nums)-1 and nums[i] == nums[i-1]:
+                i+=1
         return output
