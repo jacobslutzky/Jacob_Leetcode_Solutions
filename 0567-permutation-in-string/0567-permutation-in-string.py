15a91d6decef89adbb6b47d2999306f19a560eec
jacobslutzky <69685261+jacobslutzky@users.noreply.github.com>
2024-04-24 12:59:46 -0400

    Time: 55 ms (77.5%), Space: 16.8 MB (32.92%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,39 +1,44 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1):
-            return False
-        d = {}
-        for char in s1:
-            if char in d:
-                d[char]+=1
-            else:
-                d[char] = 1
         
-        for char in range(len(s1)):
-            if s2[char] in d:
-                d[s2[char]] -= 1
-                if d[s2[char]] == 0:
-                    del d[s2[char]]
-            else:
-                d[s2[char]] = -1
-        if not d:
-            return True
+        window = {}
 
-        for i in range(len(s2)-len(s1)):
-            if s2[i] in d:
-                d[s2[i]]+=1
-                if d[s2[i]] == 0:
-                    del d[s2[i]]
-            else:
-                d[s2[i]] = 1
-            end = i+len(s1)
-            if s2[end] in d:
-                d[s2[end]] -= 1
-                if d[s2[end]] == 0:
-                    del d[s2[end]]
-            else:
-                d[s2[end]] = -1
-            if not d:
-                return True
+        if len(s1) > len(s2):
+            return False
         
+        for char in s1:
+            if char in window:
+                window[char] +=1
+            else:
+                window[char] = 1
+        
+        for char in s2[:len(s1)]:
+            if char in window:
+                window[char]-=1
+                if window[char] == 0:
+                    del window[char]
+            else:
+                window[char]=-1
+        if not window:
+            return True
+        for i in range(len(s2)-len(s1)):
+            start = s2[i]
+            end = s2[i + len(s1)]
+            if not window:
+                return True
+            if start in window:
+                window[start] += 1
+                if window[start] == 0:
+                    del window[start]
+            else:
+                window[start] = 1
+            if end in window:
+                window[end] -= 1
+                if window[end] == 0:
+                    del window[end]
+            else:
+                window[end] = -1
+            
+            if not window:
+                return True
         return False
