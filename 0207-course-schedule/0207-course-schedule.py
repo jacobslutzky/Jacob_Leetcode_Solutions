f5d529d907de9aa4a0249ee128a41d0c1a2d5bb1
jacobslutzky <69685261+jacobslutzky@users.noreply.github.com>
2024-04-25 15:44:38 -0400

    Time: 94 ms (34.32%), Space: 18.7 MB (11.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -7,26 +7,27 @@ class Solution:
                 courses[course].append(prereq)
             else:
                 courses[course] = [prereq]
-        visited = set()
-        curr_visited = set()
-        def detect_cycle(course):
-            if course in visited:
+        
+        cycle = [0]*numCourses
+        visited = [0]*numCourses
+        def dfs(course):
+            if cycle[course]:
                 return False
-            if course in curr_visited:
+            if visited[course]:
                 return True
-            curr_visited.add(course)
+            cycle[course] = 1
+            visited[course]=1
             if course in courses:
                 for prereq in courses[course]:
-                    if detect_cycle(prereq):
-                        return True
-            curr_visited.remove(course)
-            visited.add(course)
-            return False
-            
+                    if not dfs(prereq):
+                        return False
+            cycle[course]=0
+            return True
 
-        for course in courses:
-            if course not in visited:
-                if detect_cycle(course):
-                    return False
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
         return True
 
+
+    
